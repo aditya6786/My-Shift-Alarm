759a31dc3afd44749af5a9f14c619ffb47e3729d
aditya6786 <pintusuwasiya275@gmail.com>
2026-07-24 01:46:11 +0530

    Update minSdk to 23 and Java/Kotlin versions to 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,10 @@ android {
 
     defaultConfig {
         applicationId = "com.example.myshiftalarm"
-        minSdk = 24
+        minSdk = 23
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -28,19 +26,16 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.activity:activity-ktx:1.10.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
 }
