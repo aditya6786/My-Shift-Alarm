2bdf7baa3c6b16d66702b5017e76c83de47d130d
aditya6786 <pintusuwasiya275@gmail.com>
2026-07-24 03:16:16 +0530

    Refactor BootReceiver to use restartIntent

--- app/src/main/java/com/example/myshiftalarm/BootReceiver.kt
+++ app/src/main/java/com/example/myshiftalarm/BootReceiver.kt
@@ -6,20 +6,11 @@ import android.content.Intent
 
 class BootReceiver : BroadcastReceiver() {
 
-    override fun onReceive(
-        context: Context,
-        intent: Intent
-    ) {
+    override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
-
-            val launchIntent =
-                Intent(context, MainActivity::class.java)
-
-            launchIntent.addFlags(
-                Intent.FLAG_ACTIVITY_NEW_TASK
-            )
-
-            context.startActivity(launchIntent)
+            val restartIntent = Intent(context, MainActivity::class.java)
+            restartIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            context.startActivity(restartIntent)
         }
     }
-}
+} 
